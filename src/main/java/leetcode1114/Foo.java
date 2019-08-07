7f66d15fcf7825a9c0f3ea873888d0922330ccad
yuanyawei-pbj <yuanyawei-pbj@sinosig.com>
2019-08-07 18:22:02 +0800

    1114

---
+++ src/main/java/leetcode1114/Foo.java
@@ -0,0 +1,37 @@
+package leetcode1114;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author yyw
+ * @date 2019/8/7
+ **/
+public class Foo {
+
+    private CountDownLatch countDownLatchA;
+    private CountDownLatch countDownLatchB;
+
+    public Foo() {
+        countDownLatchA = new CountDownLatch(1);
+        countDownLatchB = new CountDownLatch(1);
+    }
+
+    public void first(Runnable printFirst) {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        countDownLatchA.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        countDownLatchA.await();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        countDownLatchB.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        countDownLatchB.await();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
