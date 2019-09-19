6dad6e2fa042b1b4f65674f0be9a3ca31d5a8f26
yuanyawei-pbj <yuanyawei-pbj@sinosig.com>
2019-09-19 14:12:50 +0800

    66

--- src/main/java/leetcode66/SolutionOne.java
+++ src/main/java/leetcode66/SolutionOne.java
@@ -1,11 +1,13 @@
 package leetcode66;
 
+import java.util.Arrays;
+
 /**
  * @author yyw
  * @date 2019/9/19
  **/
 public class SolutionOne {
-    private int[] plusOne(int[] digits) {
+    private static int[] plusOne(int[] digits) {
         int length = digits.length - 1;
         for (int i = length; i >= 0; i--) {
             int sum = digits[i] + 1;
@@ -22,6 +24,7 @@ public class SolutionOne {
     }
 
     public static void main(String[] args) {
-
+        int[] a = {9, 9};
+        System.out.println(Arrays.toString(plusOne(a)));
     }
 }
