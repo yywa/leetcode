baf194e46d145ca4ec6082bef7b24ec6c5e1049a
yuanyawei-pbj <yuanyawei-pbj@sinosig.com>
2019-09-27 10:56:50 +0800

    54

---
+++ src/main/java/leetcode54/SolutionOne.java
@@ -0,0 +1,42 @@
+package leetcode54;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yyw
+ * @date 2019/9/23
+ **/
+public class SolutionOne {
+    private static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix.length == 0) {
+            return res;
+        }
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            res.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]) {
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        spiralOrder(matrix);
+    }
+}
