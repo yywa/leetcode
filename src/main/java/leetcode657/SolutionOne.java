1445e097504a1c894e49ef66447890e318169ef3
yuanyawei-pbj@sinosig.com <yuanyawei-pbj@sinosig.com>
2019-10-11 16:04:41 +0800

    657

---
+++ src/main/java/leetcode657/SolutionOne.java
@@ -0,0 +1,32 @@
+package leetcode657;
+
+import java.util.Stack;
+
+/**
+ * @author yyw
+ * @date 2019/10/11
+ */
+public class SolutionOne {
+    private static boolean judgeCircle(String moves) {
+        int lResult = 0;
+        int uResult = 0;
+        char[] chars = moves.toCharArray();
+        for (char c : chars) {
+            if (c == 'L') {
+                lResult++;
+            } else if (c == 'R') {
+                lResult--;
+            } else if (c == 'U') {
+                uResult++;
+            } else {
+                uResult--;
+            }
+
+        }
+        return lResult == 0 && uResult == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(judgeCircle("LLLRRRRUUD"));
+    }
+}
