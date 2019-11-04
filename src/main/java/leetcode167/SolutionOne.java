91fa9a8d07b27f3f85cfe045fa2c7bd7039ec26c
yywa <yuanyawei-pbj@sinosig.com>
2019-11-04 10:57:52 +0800

    167

---
+++ src/main/java/leetcode167/SolutionOne.java
@@ -0,0 +1,30 @@
+package leetcode167;
+
+import java.util.Arrays;
+
+/**
+ * @author yyw
+ * @date 2019/11/4
+ **/
+public class SolutionOne {
+    private static int[] twoSum(int[] numbers, int target) {
+        int low = 0, high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low + 1, high + 1};
+            } else if (sum < target) {
+                ++low;
+            } else {
+                --high;
+            }
+        }
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+}
