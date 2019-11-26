7b35a71d63667d5e265918ae2892e2027fbbe014
yywa <yuanyawei-pbj@sinosig.com>
2019-11-26 14:07:38 +0800

    870

---
+++ src/main/java/leetcode870/SolutionOne.java
@@ -0,0 +1,48 @@
+package leetcode870;
+
+import java.util.*;
+
+/**
+ * @author yyw
+ * @date 2019/11/26
+ **/
+public class SolutionOne {
+    private static int[] advantageCount(int[] A, int[] B) {
+        int[] sortedA = A.clone();
+        Arrays.sort(sortedA);
+        int[] sortedB = B.clone();
+        Arrays.sort(sortedB);
+
+        Map<Integer, Deque<Integer>> assigned = new HashMap<>();
+        for (int b : B) {
+            assigned.put(b, new LinkedList<>());
+        }
+
+        Deque<Integer> remaining = new LinkedList<>();
+
+        int j = 0;
+        for (int a : sortedA) {
+            if (a > sortedB[j]) {
+                assigned.get(sortedB[j++]).add(a);
+            } else {
+                remaining.add(a);
+            }
+        }
+
+        int[] ans = new int[B.length];
+        for (int i = 0; i < B.length; ++i) {
+            if (assigned.get(B[i]).size() > 0) {
+                ans[i] = assigned.get(B[i]).pop();
+            } else {
+                ans[i] = remaining.pop();
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {2, 7, 11, 15};
+        int[] B = {1, 10, 4, 11};
+        System.out.println(Arrays.toString(advantageCount(A, B)));
+    }
+}
