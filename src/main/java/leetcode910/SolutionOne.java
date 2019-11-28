759772a359843057c66ad3d7437beb3ce85c12f4
yywa <yuanyawei-pbj@sinosig.com>
2019-11-28 11:07:45 +0800

    910

---
+++ src/main/java/leetcode910/SolutionOne.java
@@ -0,0 +1,20 @@
+package leetcode910;
+
+
+
+/**
+ * @author yyw
+ * @date 2019/11/28
+ **/
+public class SolutionOne {
+    private static int smallestRangeII(int[] A, int K) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 3, 6};
+        int K = 3;
+        System.out.println(smallestRangeII(A, K));
+    }
+}
