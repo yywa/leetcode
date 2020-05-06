123d0a37fc2063c09ba3ea0fc67f89696ef9da33
yywa <yuanyawei-pbj@sinosig.com>
2020-05-06 15:46:01 +0800

    45

---
+++ src/main/java/leetcode45/SolutionOne.java
@@ -0,0 +1,28 @@
+package leetcode45;
+
+/**
+ * @author yyw
+ * @date 2020/5/6
+ * @description
+ */
+public class SolutionOne {
+    public static int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(jump(nums));
+    }
+}
