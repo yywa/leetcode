7613a535cf5bcf2f46315d4727926a8b1e268436
yywa <yuanyawei-pbj@sinosig.com>
2020-05-06 15:36:05 +0800

    53

---
+++ src/main/java/leetcode53/SolutionOne.java
@@ -0,0 +1,22 @@
+package leetcode53;
+
+/**
+ * @author yyw
+ * @date 2020/5/6
+ * @description
+ */
+public class SolutionOne {
+    public static int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+}
