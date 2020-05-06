fda73fbec1ef9b6bb17d705e4aaeb8c2ba761431
yywa <yuanyawei-pbj@sinosig.com>
2020-05-06 15:27:52 +0800

    3

---
+++ src/main/java/leetcode3/SolutionOne.java
@@ -0,0 +1,37 @@
+package leetcode3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author yyw
+ * @date 2020/5/6
+ * @description
+ */
+public class SolutionOne {
+
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, result = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            result = Math.max(result, rk - i + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+}
