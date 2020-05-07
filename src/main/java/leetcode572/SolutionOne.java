022a6faf453c78861f316253da97ff75471e12ea
yywa <yuanyawei-pbj@sinosig.com>
2020-05-07 09:31:31 +0800

    572

---
+++ src/main/java/leetcode572/SolutionOne.java
@@ -0,0 +1,41 @@
+package leetcode572;
+
+/**
+ * @author yyw
+ * @date 2020/5/7
+ * @description
+ */
+public class SolutionOne {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (t == null) {
+            return true;
+        }
+        if (s == null) {
+            return false;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t) || isSameTree(s, t);
+    }
+
+    public boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
