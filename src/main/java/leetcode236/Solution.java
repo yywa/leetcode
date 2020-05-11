2cc4e740d294e0051b4e89965018100db7785206
yywa <yuanyawei-pbj@sinosig.com>
2020-05-11 10:47:46 +0800

    236

---
+++ src/main/java/leetcode236/Solution.java
@@ -0,0 +1,43 @@
+package leetcode236;
+
+/**
+ * @author yyw
+ * @date 2020/5/11
+ * @description 二叉树的最近公共祖先
+ */
+public class Solution {
+    private TreeNode ans;
+
+    public Solution() {
+        this.ans = null;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return false;
+        }
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
