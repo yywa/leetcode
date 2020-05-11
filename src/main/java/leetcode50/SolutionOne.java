851f5d2f9a08ec9c853acab214cbdd47da187fde
yywa <yuanyawei-pbj@sinosig.com>
2020-05-11 09:46:29 +0800

    50

---
+++ src/main/java/leetcode50/SolutionOne.java
@@ -0,0 +1,21 @@
+package leetcode50;
+
+/**
+ * @author yyw
+ * @date 2020/5/11
+ * @description
+ */
+public class SolutionOne {
+    public static double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+    public static double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+}
