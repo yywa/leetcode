3003435002c8471dc5bc26df1eac6496e9290b9a
yywa <yuanyawei-pbj@sinosig.com>
2020-05-29 10:04:42 +0800

    198

---
+++ src/main/java/leetcode198/SolutionOne.java
@@ -0,0 +1,31 @@
+package leetcode198;
+
+/**
+ * @author yyw
+ * @date 2020/5/29
+ * @description   打家劫舍
+ */
+public class SolutionOne {
+    public static int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(nums[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 9, 3, 1};
+        System.out.println(rob(nums));
+    }
+}
