02142027892183a9692398467afbf4bcf6ff45a4
yywa <yuanyawei-pbj@sinosig.com>
2020-06-08 09:23:32 +0800

    990

---
+++ src/main/java/leetcode990/SolutionOne.java
@@ -0,0 +1,50 @@
+package leetcode990;
+
+/**
+ * @author yyw
+ * @date 2020/6/8
+ * @description   等式方程的可满足性
+ */
+public class SolutionOne {
+    public static boolean equationsPossible(String[] equations) {
+        int[] parent = new int[26];
+        for (int i = 0; i < 26; i++) {
+            parent[i] = i;
+        }
+        for (String str : equations) {
+            if (str.charAt(1) == '=') {
+                int index1 = str.charAt(0) - 'a';
+                int index2 = str.charAt(3) - 'a';
+                union(parent, index1, index2);
+            }
+        }
+        for (String str : equations) {
+            if (str.charAt(1) == '!') {
+                int index1 = str.charAt(0) - 'a';
+                int index2 = str.charAt(3) - 'a';
+                if (find(parent, index1) == find(parent, index2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public static int find(int[] parent, int index) {
+        while (parent[index] != index) {
+            parent[index] = parent[parent[index]];
+            index = parent[index];
+        }
+        return index;
+    }
+
+
+    public static void main(String[] args) {
+        String[] s = {"a!=a"};
+        System.out.println(equationsPossible(s));
+    }
+}
